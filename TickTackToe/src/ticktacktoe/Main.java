5fe5149f9b3fa03181682c844eabe6b3a08fbac1
iaroslavgladkyi <igladkyi@gmail.com>
2016-05-09 15:03:56 +0300

    Add two char variables

--- TickTackToe/src/ticktacktoe/Main.java
+++ TickTackToe/src/ticktacktoe/Main.java
@@ -15,7 +15,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        final char x = 'x';
+        final char o = 'o';
+        
+       
+        
     }
     
 }
